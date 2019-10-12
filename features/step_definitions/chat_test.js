19d41591801b8271e7cae89be30af4048d4d6b97
gleiderr <gleiderr@gmail.com>
2019-10-12 17:16:01 -0300

    Polyfill de requestAnimationFrame

--- features/step_definitions/chat_test.js
+++ features/step_definitions/chat_test.js
@@ -13,6 +13,9 @@ import { Simulate, act } from 'react-dom/test-utils';
 const {window} = new JSDOM(`<!DOCTYPE html><body></body></html>`);
 global.window = window;
 global.document = window.document;
+//Polyfill de requestAnimationFrame
+global.requestAnimationFrame =  window.requestAnimationFrame = 
+  callback => setTimeout(callback, 0);
 
 let mensagens;
 let listeners = {};
