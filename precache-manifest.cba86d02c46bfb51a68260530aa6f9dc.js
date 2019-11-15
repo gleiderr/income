8355e741d47d78ca6d6847a4e8be6e8a1d6f119c
gleiderr <gleiderr@gmail.com>
2019-11-15 08:15:19 -0300

    Updates

---
+++ precache-manifest.cba86d02c46bfb51a68260530aa6f9dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61be808510034d7734dca9b33d06f38a",
+    "url": "/income/index.html"
+  },
+  {
+    "revision": "6ceba7241f8d120ccf3f",
+    "url": "/income/static/css/2.6332908a.chunk.css"
+  },
+  {
+    "revision": "e2d8903fa87b82fbac6d",
+    "url": "/income/static/css/main.da5bfd99.chunk.css"
+  },
+  {
+    "revision": "6ceba7241f8d120ccf3f",
+    "url": "/income/static/js/2.7cdaa1a9.chunk.js"
+  },
+  {
+    "revision": "e2d8903fa87b82fbac6d",
+    "url": "/income/static/js/main.5d773656.chunk.js"
+  },
+  {
+    "revision": "5e33b9d9afb15124d2ac",
+    "url": "/income/static/js/runtime-main.29e074b0.js"
+  }
+]);
