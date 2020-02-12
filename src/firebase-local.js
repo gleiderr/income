9a40462d6706f2d8b5e0351224a48ea2ddd18b6e
Gleider Costa - Getin <gleider.costa@alterosa.CONSERVO>
2020-02-12 12:29:52 -0300

    Incluída configuração de teste

--- src/firebase-local.js
+++ src/firebase-local.js
@@ -1,4 +1,6 @@
 import firebase from "firebase";
+//aguardando uma próxima versão que trabalhe do lado do cliente e não somente do lado do servidor como essa versão 0.16.8
+//import {initializeTestApp} from "@firebase/testing";
 
 let app = undefined;
 export default function firebase_init() {
@@ -13,6 +15,24 @@ export default function firebase_init() {
     messagingSenderId: "868861057308",
     appId: "1:868861057308:web:72ab5d4b1e875ce7"
   };
+
   // Initialize Firebase
-  return app = firebase.initializeApp(firebaseConfig);
+  app = firebase.initializeApp(firebaseConfig);
+
+  /**
+   * $ npm install -g firebase-tools
+   * $ firebase loging
+   * $ firebase emulators:start --only firestore,functions
+   *
+   * https://firebase.google.com/docs/rules/unit-tests
+   * https://firebase.google.com/docs/firestore/security/test-rules-emulator
+   * https://github.com/firebase/quickstart-nodejs/tree/master/firestore-emulator/javascript-quickstart
+   */
+  if (window.location.hostname !== 'gleider.ml') {
+    app.firestore().settings({
+      host: 'localhost:3081',
+      ssl: false,
+    });
+  }
+  return app;
 }
