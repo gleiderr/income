7c0a2829a3c5bc51c0cd5786b2713e0959827f9f
Gleider Costa - Getin <gleider.costa@alterosa.CONSERVO>
2020-02-18 12:57:10 -0300

    Novo link a ferramenta útil

--- src/firebase-local.js
+++ src/firebase-local.js
@@ -36,6 +36,7 @@ export default function firebase_init() {
    * https://firebase.google.com/docs/rules/unit-tests
    * https://firebase.google.com/docs/firestore/security/test-rules-emulator
    * https://github.com/firebase/quickstart-nodejs/tree/master/firestore-emulator/javascript-quickstart
+   * https://github.com/firebase/firebase-tools-ui
    */
   if (window.location.hostname !== 'gleider.ml') {
     app.firestore().settings({
